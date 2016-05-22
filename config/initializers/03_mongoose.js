535b39d37f8eb556278f2e82c3f7abb9075dcbc0
giftofjehovah <jonathan.pwh@gmail.com>
2016-05-22 22:10:13 +0800

    change mongouri

--- config/initializers/03_mongoose.js
+++ config/initializers/03_mongoose.js
@@ -6,7 +6,7 @@ module.exports = function () {
       mongoose.connect('mongodb://localhost:27017/studentDashboard')
       break
     case 'production':
-      mongoose.connect(process.env.MONGOLAB_URI)
+      mongoose.connect(process.env.MONGODB_URI)
       break
   }
 }
